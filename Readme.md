0149f362f8f746c5b0c58b82728731a4d87c8038
Tanu <21bcs055@nith.ac.in>
2025-07-23 18:31:46 +0530

    connect to mongo atlas

--- Readme.md
+++ Readme.md
@@ -3,5 +3,3 @@
 This is a practice on backend with javascript
 
 - [Model link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj?origin=share)
-
-
---
+++ src/index.js
@@ -0,0 +1,41 @@
+//require('dotenv').config({path:'./env'}) //we don't prefer to write this because of inconsistency in code
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+/*
+//connecting database using IIFE method (professional approach)
+//semicolon is put before for the cleaning process
+
+
+
+import express from "express"
+const app = express()
+;(asyn () => {
+    try{
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        application.on("error",(error) => {
+            console.log("ERROR: ",error);
+            throw error
+        })
+
+        application.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    }catch (error) {
+        console.log("ERROR: ",error)
+        throw err
+    }
+})()
+    */
